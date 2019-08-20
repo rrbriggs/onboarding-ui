7385a220b21a2d4612d5110a88b4ccf8784efa37
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-08-19 21:26:54 -0500

    breakout each post into its own div

--- src/js/page.js
+++ src/js/page.js
@@ -9,9 +9,55 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // "load" = transfer is complete, all data is now in response
     request.addEventListener("load", function() {
+        // todo: work on display data
+
         console.log(this.response);
-        let textNode = document.createTextNode(this.response[0]["message"]);
-        appendTimelineDataBlob(textNode, timelineDiv);
+ 
+        // set up new div per tweet/post
+        var toAdd = document.createDocumentFragment();
+        for(let x = 0; x < this.response.length; x++) {
+            let newDiv = document.createElement("div");
+            newDiv.id = x;
+            
+            // make each new post div a clickable link to the post itself
+            let handle = this.response[x]["socialUser"]["twitterHandle"];
+            let postID = this.response[x]["postID"];
+            newDiv.addEventListener("click", function() {
+                location.href = `http://twitter.com/${handle}/status/${postID}`;
+            });
+
+            // alternating div colors
+            if (x % 2 == 0) {
+                newDiv.style.backgroundColor = "LightCyan";
+            } else {
+                newDiv.style.backgroundColor = "LightBlue";
+            }
+
+            let newPhotoSpan = document.createElement("IMG");
+            newPhotoSpan.id = x;
+            newPhotoSpan.src = this.response[x]["socialUser"]["profileImageUrl"];
+            //let photo = document.createTextNode(this.response[x]["socialUser"]["profileImageUrl"]);
+            //newPhotoSpan.appendChild(photo);
+            newDiv.appendChild(newPhotoSpan);
+
+            let newMessageSpan = document.createElement("span");
+            newMessageSpan.id = x;
+            let message = document.createTextNode(this.response[x]["message"]);
+            newMessageSpan.appendChild(message);
+            newDiv.appendChild(newMessageSpan);
+
+            let newDateSpan = document.createElement("span");
+            newDateSpan.id = x;
+            let epochDate = parseInt(this.response[x]["createdAt"]);
+            let readableDate = new Date(epochDate);
+            let date = document.createTextNode(readableDate);
+            newDateSpan.appendChild(date);
+            newDiv.appendChild(newDateSpan);
+
+            toAdd.appendChild(newDiv);
+        }
+
+        timelineDiv.append(toAdd);
     });
 
     button.addEventListener("error", function() {
@@ -21,7 +67,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // put data into div 
     const appendTimelineDataBlob = function(textNode, container) {
-        const timelineDataBlob = document.createElement("div1").appendChild(textNode);
+        const timelineDataBlob = document.createElement("div").appendChild(textNode);
         container.append(timelineDataBlob);
     };
 
