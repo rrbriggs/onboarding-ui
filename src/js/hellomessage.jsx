2ccc971a3dd2ba3a4ebc383c2866741250de157b
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-08-27 14:04:21 -0500

    slight refactor

--- src/js/hellomessage.jsx
+++ src/js/hellomessage.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Hello extends React.Component {
+class HelloReact extends React.Component {
     render() {
-        return <div>Hello</div>;
+        return (<div>Hello</div>);
     }
 }
 
-export default Hello;
-//module.hot.accept();
+export default HelloReact;
