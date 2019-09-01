a9db9f49b6c681c07bedc2be6c5e25c54ea60e41
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-09-01 18:39:15 -0500

    posts are now reactish

---
+++ src/js/react/PostFactoryComponent.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PhotoContainer from './PhotoContainer';
+import MessageContainer from './MessageContainer';
+
+// todo: make oddPostBlock set programmatically
+class PostFactoryComponent extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            photoURL: this.props.photoURL,
+            screenName: this.props.screenName,
+            userHandle: this.props.userHandle,
+            date: this.props.date,
+            statusMessage: this.props.statusMessage
+        }
+    }
+
+    render() {
+        return(
+            <div className='oddPostBlock postStyle'>
+                <PhotoContainer photoURL={this.state.photoURL} screenName={this.state.screenName} userHandle={this.state.userHandle} />
+                <MessageContainer date={this.state.date} statusMessage={this.state.statusMessage} />
+            </div>
+        )
+    }
+}
+
+export default PostFactoryComponent;
