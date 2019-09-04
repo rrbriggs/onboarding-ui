2973b8fd864560f4df161b83cd3cc5858c1262ce
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-09-04 10:38:36 -0500

    rename testStatus to callback

--- src/js/react/TimelineRequest.js
+++ src/js/react/TimelineRequest.js
@@ -1,8 +1,8 @@
-export default function timelineRequest(testStatus) {
+export default function timelineRequest(callback) {
     const request = new XMLHttpRequest();
 
     request.addEventListener("load", () => {
-        testStatus(request.response);
+        callback(request.response);
     });
 
     request.open("GET", "http://localhost:8080/api/1.0/twitter/timeline");
