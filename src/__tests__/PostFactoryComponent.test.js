6fdd117c2481fa45fee7501e0cdefa0e1df5d8ba
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-09-11 11:16:47 -0500

    Add PostFactoryComponent test

---
+++ src/__tests__/PostFactoryComponent.test.js
@@ -0,0 +1,7 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import PostFactoryComponent from '../js/react/PostFactoryComponent';
+import PhotoContainer from '../js/react/PhotoContainer';
+import MessageContainer from './js/react/MessageContainer';
+
+describe('')
