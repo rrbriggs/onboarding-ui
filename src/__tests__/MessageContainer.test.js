afa763139e27dfa3bd79c19bd6cb5e1ee4535c48
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-09-11 23:17:17 -0500

    add MessageContainer tests

---
+++ src/__tests__/MessageContainer.test.js
@@ -0,0 +1,24 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import MessageContainer from '../js/react/MessageContainer';
+import DateComponent from '../js/react/DateComponent';
+import MessageComponent from '../js/react/MessageComponent';
+
+describe('test MessageContainer', () => {
+    const data = {
+        date: Date.now().valueOf(),
+        statusMessage: "message"
+    }
+
+    const messageContainer = shallow(< MessageContainer date={data.date} statusMessage={data.statusMessage} />);
+
+    it('renders without crashing', () => {
+        messageContainer;
+    });
+
+    it('render contains components', () => {
+        expect(messageContainer.find('.photoContainer')).toBeTruthy();
+        expect(messageContainer.childAt(0)).toContainEqual(<DateComponent date={data.date} />);
+        expect(messageContainer.childAt(1)).toContainEqual(<MessageComponent statusMessage={data.statusMessage} />);
+    })
+});
