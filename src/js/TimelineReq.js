eb37158c28a2afa2e37806224e9930c1db52dabc
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-09-23 14:42:20 -0500

    timeline request as a promise unchecked

--- src/js/TimelineReq.js
+++ src/js/TimelineReq.js
@@ -1,4 +1,9 @@
-const timeline = () => {
-    let promise = new Promise()
-
+function timeline(callback) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", "http://localhost:8080/api/1.0/twitter/timeline");
+        xhr.onload = () => resolve(callback(xhr.response));
+        xhr.onerror = () => reject(callback(false));
+        xhr.send();
+    });
 }
