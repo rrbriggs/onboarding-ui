cf380c70e9b786c5b6c1974c994f450032959103
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-09-26 11:18:07 -0500

    update to use .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -33,9 +33,9 @@ module.exports = {
             },
             use: {
                 loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-react']
-                }
+                // options: {
+                //     presets: ['@babel/preset-react', '@babel/preset-env']
+                // }
             }
         }]
     }
