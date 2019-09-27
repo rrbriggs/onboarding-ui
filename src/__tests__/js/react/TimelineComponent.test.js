b43d0acca0add9f7d6de689afb82d01bbb2c6657
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-09-26 19:07:18 -0500

    fix tests for promises

--- src/__tests__/js/react/TimelineComponent.test.js
+++ src/__tests__/js/react/TimelineComponent.test.js
@@ -10,49 +10,52 @@ jest.mock('../../../js/TimelineReq');
 
 describe('test TimelineComponent', () => {
 
-    timelineReq.mockImplementation(() => {
-        return new Promise((resolve, reject) => {
-            reject(false);
-        })
-    });
+    // timelineReq.mockImplementation(() => {
+    //     return new Promise((resolve, reject) => {
+    //         reject(false);
+    //     })
+    // });
+
+    timelineReq.mockResolvedValue(false);
 
     const timelineComponent = shallow(< TimelineComponent />);
 
     it('checks for error case', () => {
-        // timelineReq.mockImplementation(() => "");
         expect(timelineComponent.contains('No data currently available.')).toBeTruthy();
     });
 
     it('button click executes function that calls timelineReq with parseDataJson as callback', () => {
-        // timelineComponent
-        //     .find('button')
-        //     .simulate("click");
-        // expect(timelineReq).toHaveBeenCalledWith(timelineComponent.instance());
+        timelineComponent
+            .find('button')
+            .simulate("click");
+
+        //timelineReq is already called once one mount
+        expect(timelineReq).toHaveBeenCalledTimes(2);
     });
 
     it('button renders data from PostFactoryComponent', () => {
-        // const data = {
-        //     key: "1",
-        //     postID: "1234",
-        //     date: Date.now().valueOf(),
-        //     statusMessage: "message",
-        //     postStyle: "evenPostBlock",
-        //     socialUser: {
-        //         photoURL: "www.twitter.com",
-        //         screenName: "screen name",
-        //         handle: "handle"
-        //     }
-        // }
+        const data = {
+            key: "1",
+            postID: "1234",
+            date: Date.now().valueOf(),
+            statusMessage: "message",
+            postStyle: "evenPostBlock",
+            socialUser: {
+                photoURL: "www.twitter.com",
+                screenName: "screen name",
+                handle: "handle"
+            }
+        }
 
-        // timelineReq.mockImplementation(() => {
-        //     timelineComponent.instance()
-        //             .parseDataJson(JSON.stringify([data, data, data, data]));
-        // });
+        timelineReq.mockImplementation(() => {
+            timelineComponent.instance()
+                    .parseDataJson(JSON.stringify([data, data, data, data]));
+        });
 
-        // timelineComponent
-        //     .find('button')
-        //     .simulate("click");
+        timelineComponent
+            .find('button')
+            .simulate("click");
 
-        // expect(timelineComponent.containsMatchingElement(<PostFactoryComponent />)).toBeTruthy();
+        expect(timelineComponent.containsMatchingElement(<PostFactoryComponent />)).toBeTruthy();
     });
 });
