71fd3a6e4e1cdc129d1b673e7c0d629bffa2178c
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-09-30 17:19:03 -0500

    update for endpoint change

--- src/js/TimelineReq.js
+++ src/js/TimelineReq.js
@@ -11,7 +11,7 @@ export async function timelineReq() {
 export async function userTimelineReq() {
     return await new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://localhost:8080/api/1.0/twitter/timeline?userTimeline=user");
+        xhr.open("GET", "http://localhost:8080/api/1.0/twitter/userTimeline");
         xhr.onload = () => resolve(xhr.response);
         xhr.onerror = () => reject(false);
         xhr.send();
