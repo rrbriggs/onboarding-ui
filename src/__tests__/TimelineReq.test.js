d9aa5d44e58f1f680cccdfadc5cd7cfc8c7a8d56
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-09-30 20:46:15 -0500

    add tests for new ui features

--- src/__tests__/TimelineReq.test.js
+++ src/__tests__/TimelineReq.test.js
@@ -1,10 +1,18 @@
-import timelineReq from '../js/TimelineReq';
+import {timelineReq} from '../js/TimelineReq';
+import {userTimelineReq} from '../js/TimelineReq';
 jest.mock('../js/TimelineReq');
 
 
 describe('testing timelineReq', () => {
     it('returns promise', async () => {
         const data = await timelineReq();
+        return expect(data).toBeFalsy();
+    });
+});
+
+describe('testing userTimelineReq', () => {
+    it('returns promise', async () => {
+        const data = await userTimelineReq();
         return expect(data).toBeFalsy();
     });
 });
--- src/__tests__/js/react/TimelineComponent.test.js
+++ src/__tests__/js/react/TimelineComponent.test.js
@@ -1,7 +1,8 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 import TimelineComponent from '../../../js/react/TimelineComponent';
-import timelineReq from '../../../js/TimelineReq';
+import {timelineReq} from '../../../js/TimelineReq';
+import {userTimelineReq} from '../../../js/TimelineReq';
 import PostFactoryComponent from '../../../js/react/PostFactoryComponent';
 
 jest.mock('../../../js/TimelineReq');
@@ -11,6 +12,7 @@ jest.mock('../../../js/TimelineReq');
 describe('test TimelineComponent', () => {
 
     timelineReq.mockResolvedValue(false);
+    userTimelineReq.mockResolvedValue(false);
 
     const timelineComponent = shallow(< TimelineComponent />);
 
@@ -18,16 +20,25 @@ describe('test TimelineComponent', () => {
         expect(timelineComponent.contains('No data currently available.')).toBeTruthy();
     });
 
-    it('button click executes function that calls timelineReq with parseDataJson as callback', () => {
+    it('getTimelineButton click executes function that calls timelineReq with parseDataJson as callback', () => {
         timelineComponent
-            .find('button')
+            .find('#getTimelineButton')
             .simulate("click");
 
         //timelineReq is already called once one mount
         expect(timelineReq).toHaveBeenCalledTimes(2);
     });
 
-    it('button renders data from PostFactoryComponent', () => {
+    it('getUserTimelineButton click executes function that calls userTimelineReq with parseUserTimeline as callback', () => {
+        timelineComponent
+            .find('#getUserTimelineButton')
+            .simulate("click");
+
+        //timelineReq is already called once one mount
+        expect(userTimelineReq).toHaveBeenCalledTimes(2);
+    });
+
+    it('getTimelineButton renders data from PostFactoryComponent', () => {
         const data = {
             key: "1",
             postID: "1234",
@@ -47,7 +58,33 @@ describe('test TimelineComponent', () => {
         });
 
         timelineComponent
-            .find('button')
+            .find('#getTimelineButton')
+            .simulate("click");
+
+        expect(timelineComponent.containsMatchingElement(<PostFactoryComponent />)).toBeTruthy();
+    });
+
+    it('getUserTimelineButton renders data from PostFactoryComponent', () => {
+        const data = {
+            key: "1",
+            postID: "1234",
+            date: Date.now().valueOf(),
+            statusMessage: "message",
+            postStyle: "evenPostBlock",
+            socialUser: {
+                photoURL: "www.twitter.com",
+                screenName: "screen name",
+                handle: "handle"
+            }
+        }
+
+        userTimelineReq.mockImplementation(() => {
+            timelineComponent.instance()
+                    .parseUserTimeline(JSON.stringify([data, data, data, data]));
+        });
+
+        timelineComponent
+            .find('#getUserTimelineButton')
             .simulate("click");
 
         expect(timelineComponent.containsMatchingElement(<PostFactoryComponent />)).toBeTruthy();
